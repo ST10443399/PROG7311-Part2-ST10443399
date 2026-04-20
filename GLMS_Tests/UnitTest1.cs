addd9b06e167d83864ffcdb1a7e924bae49efe25
ST10443399 <161062694+ST10443399@users.noreply.github.com>
2026-04-20 21:26:46 +0200

    Add initial solution, web app and test project
    Add new ASP.NET web application PROG7311_ST10443399_POE and accompanying GLMS_Tests xUnit project. Includes solution file, Program.cs, controllers, models, views, appsettings, launchSettings, wwwroot static assets (Bootstrap/jQuery), project files (.csproj) and a test stub UnitTest1 with test dependencies (xunit, Microsoft.NET.Test.Sdk, coverlet). Also adds Visual Studio workspace and build artifacts (.vs/, obj/) generated by the IDE.

---
+++ GLMS_Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace GLMS_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ PROG7311_ST10443399_POE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG7311_ST10443399_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
